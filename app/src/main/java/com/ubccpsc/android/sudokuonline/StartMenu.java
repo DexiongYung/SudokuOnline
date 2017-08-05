3f42a9738e93ff34298d6dba9de96eca27d1ea7b
Dylan <Yungdexiong@hotmail.com>
2017-08-05 16:18:43 -0700

    mfgravesjr grid generator and main menu layout done Added layouts

--- app/src/main/java/com/ubccpsc/android/sudokuonline/StartMenu.java
+++ app/src/main/java/com/ubccpsc/android/sudokuonline/StartMenu.java
@@ -2,12 +2,18 @@ package com.ubccpsc.android.sudokuonline;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
-public class StartMenu extends AppCompatActivity {
+public class StartMenu extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_start_menu);
+    }
+
+    @Override
+    public void onClick(View v) {
+
     }
 }
