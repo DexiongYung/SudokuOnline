41c018fcbb9745f0df23dac94e77b2764673a665
Adi <aditya.24@hotmail.com>
2017-08-19 16:34:42 -0700

    Drawers raided

--- app/src/main/java/com/ubccpsc/android/sudokuonline/Grid.java
+++ app/src/main/java/com/ubccpsc/android/sudokuonline/Grid.java
@@ -2,8 +2,13 @@ package com.ubccpsc.android.sudokuonline;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import java.util.Random;
 
@@ -14,10 +19,46 @@ import SudokuGenerator.GameEngine;
  */
 
 public class Grid extends AppCompatActivity implements View.OnClickListener {
+
+    private String[] mPlanetTitles;
+    private DrawerLayout mDrawerLayout;
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
+
+
     @Override
     protected void onCreate(Bundle savedStateInstance){
         super.onCreate(savedStateInstance);
         setContentView(R.layout.grid);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+        // Set the adapter for the list view
+        addDrawerItems();
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                switch(position){
+                    case 0:{
+                        Intent myIntent = new Intent(Grid.this, StartMenu.class);
+                        startActivity(myIntent);
+                        break;}
+                    case 1:{
+
+                        break;}
+                    case 2:{
+                        break;}
+                    case 3:{
+                        startActivity(new Intent(Grid.this,Grid.class));
+                        finish();
+                        break;}
+                    case 4:{
+
+                        break;}
+                }
+            }
+        });
+
+
 
         Intent myIntent = getIntent();
 
@@ -50,5 +91,11 @@ public class Grid extends AppCompatActivity implements View.OnClickListener {
     @Override
     public void onClick(View v) {
 
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Home", "Increase Difficulty", "Decrease Difficulty", "New Game", "Clear Grid" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
     }
 }
