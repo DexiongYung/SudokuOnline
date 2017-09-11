e8939e20f6e2845d54aea479a50bc4ed81cc341a
Dylan <Yungdexiong@hotmail.com>
2017-09-11 11:19:23 -0700

    refactored. Saved Game onDestroy and Themes remaining

--- app/src/main/java/SudokuGenerator/GameEngine.java
+++ app/src/main/java/SudokuGenerator/GameEngine.java
@@ -7,6 +7,7 @@ import java.util.ArrayList;
 import java.util.Stack;
 
 import View.SudokuGrid.GameGrid;
+import View.SudokuGrid.SudokuCell;
 
 /**
  * Created by Dylan on 2017-08-07.
@@ -17,13 +18,11 @@ public class GameEngine {
     private GameGrid grid = null;
     private int selectedPosX = -1, selectedPosY = -1;
     private boolean draftMode;
-    private Stack<Integer> undoStorage = new Stack<>();
-    private Stack<Integer> redoStorage = new Stack<>();
-    private ArrayList<Integer> highlightDuplicatesPosition = new ArrayList<>();
-    private ArrayList<Integer> highlightRowsPosition = new ArrayList<>();
-    private ArrayList<Integer> hightlightColsPosition = new ArrayList<>();
-    private ArrayList<Integer> highlightRegionPosition = new ArrayList<>();
+    private Stack<Integer> undoStorage = new Stack<>(), redoStorage = new Stack<>();
+    private ArrayList<Integer> highlightDuplicatesPosition = new ArrayList<>(), highlightRowsPosition = new ArrayList<>(),
+            hightlightColsPosition = new ArrayList<>(), highlightRegionPosition = new ArrayList<>();
     private int level;
+
     private GameEngine() {
     }
 
@@ -71,15 +70,16 @@ public class GameEngine {
 
     //HIGHLIGHTING RELEVANT CELLS
     private void highlightCells(int xPos, int yPos) {
+        SudokuCell[][] oSudokuCellGrid = getGameGrid().getSudokuCellGrid();
         //Sets rows, columns, regions and duplicates to white
         for (int i = 0; i < highlightRowsPosition.size(); i++) {
-            getGameGrid().getSudokuCellGrid()[highlightRowsPosition.get(i) % 9][highlightRowsPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
-            getGameGrid().getSudokuCellGrid()[hightlightColsPosition.get(i) % 9][hightlightColsPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
-            getGameGrid().getSudokuCellGrid()[highlightRegionPosition.get(i) % 9][highlightRegionPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
+            oSudokuCellGrid[highlightRowsPosition.get(i) % 9][highlightRowsPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
+            oSudokuCellGrid[hightlightColsPosition.get(i) % 9][hightlightColsPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
+            oSudokuCellGrid[highlightRegionPosition.get(i) % 9][highlightRegionPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
         }
 
         for (int i = 0; i < highlightDuplicatesPosition.size(); i++)
-            getGameGrid().getSudokuCellGrid()[highlightDuplicatesPosition.get(i) % 9][highlightDuplicatesPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
+            oSudokuCellGrid[highlightDuplicatesPosition.get(i) % 9][highlightDuplicatesPosition.get(i) / 9].setBackgroundColor(Color.WHITE);
 
         //Clears highlight info since they've been set to white
         highlightRowsPosition.clear();
@@ -92,30 +92,27 @@ public class GameEngine {
 
         //Changes color of all row and column shared in (x,y) coordinate and region
         for (int i = 0; i < 9; i++) {
-            getGameGrid().getSudokuCellGrid()[i][yPos].setBackgroundColor(Color.parseColor("#ffffe0"));
+            oSudokuCellGrid[i][yPos].setBackgroundColor(Color.parseColor("#ffffe0"));
             hightlightColsPosition.add(xPos + i * 9);
-            getGameGrid().getSudokuCellGrid()[xPos][i].setBackgroundColor(Color.parseColor("#ffffe0"));
+            oSudokuCellGrid[xPos][i].setBackgroundColor(Color.parseColor("#ffffe0"));
             highlightRowsPosition.add(9 * yPos + i);
-            getGameGrid().getSudokuCellGrid()[highlightRegionPosition.get(i) % 9][highlightRegionPosition.get(i) / 9].setBackgroundColor(Color.parseColor("#ffffe0"));
+            oSudokuCellGrid[highlightRegionPosition.get(i) % 9][highlightRegionPosition.get(i) / 9].setBackgroundColor(Color.parseColor("#ffffe0"));
         }
 
         for (int x = 0; x < 9; x++) {
             for (int y = 0; y < 9; y++) {
-                if ((getGameGrid().getGrid()[x][y] != 0) && (getGameGrid().getGrid()[x][y] != -1)) {
-                    if (getGameGrid().getGrid()[x][y] == getGameGrid().getGrid()[xPos][yPos]) {
-                        if (x == xPos || y == yPos || ((x / 3) + 3 * (y / 3) + 1) == ((xPos / 3) + 3 * (yPos / 3) + 1)) {
-                            getGameGrid().getSudokuCellGrid()[x][y].setBackgroundColor(Color.parseColor("#FF6666"));
-                        } else {
-                            getGameGrid().getSudokuCellGrid()[x][y].setBackgroundColor(Color.parseColor("#89cff0"));
-                        }
-                        highlightDuplicatesPosition.add(y * 9 + x);
-                    }
+                if ((getGameGrid().getGrid()[x][y] > 0) && (getGameGrid().getGrid()[x][y] == getGameGrid().getGrid()[xPos][yPos])) {
+                    if (x != xPos || y != yPos || ((x / 3) + 3 * (y / 3) + 1) != ((xPos / 3) + 3 * (yPos / 3) + 1))
+                        oSudokuCellGrid[x][y].setBackgroundColor(Color.parseColor("#FF6666"));
+                    else
+                        oSudokuCellGrid[x][y].setBackgroundColor(Color.parseColor("#89cff0"));
+                    highlightDuplicatesPosition.add(y * 9 + x);
                 }
             }
         }
 
         //sets (x,y) position to light pink to signify it's the cell currently selected
-        getGameGrid().getSudokuCellGrid()[xPos][yPos].setBackgroundColor(Color.parseColor("#ffb6c1"));
+        oSudokuCellGrid[xPos][yPos].setBackgroundColor(Color.parseColor("#ffb6c1"));
     }
 
     private void findRegion(int xPos, int yPos) {
@@ -201,9 +198,9 @@ public class GameEngine {
     //undo and redo functions
     public void undoFunction() {
         if (!undoStorage.isEmpty()) {
-            int temp = undoStorage.pop();
-            redoStorage.push(temp);
-            grid.getSudokuCellGrid()[temp % 9][temp / 9].undo();
+            int t = undoStorage.pop();
+            redoStorage.push(t);
+            grid.getSudokuCellGrid()[t % 9][t / 9].undo();
             highlightCells(selectedPosX, selectedPosY);
         }
     }
