6767f14d4f3046ef211a7830756ad3b90c24d482
Graham <gramclee@gmail.com>
2017-09-29 11:15:47 -0700

    Created SignInActivity class for google logins

---
+++ app/src/main/java/com/ubccpsc/android/sudokuonline/SignInActivity.java
@@ -0,0 +1,26 @@
+package com.ubccpsc.android.sudokuonline;
+
+/**
+ * Created by Graham on 2017-09-29.
+ */
+
+
+import android.support.v7.app.AppCompatActivity;
+
+import com.google.android.gms.auth.api.Auth;
+import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
+import com.google.android.gms.auth.api.signin.GoogleSignInOptions;
+import com.google.android.gms.auth.api.signin.GoogleSignInResult;
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.SignInButton;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.common.api.OptionalPendingResult;
+import com.google.android.gms.common.api.ResultCallback;
+import com.google.android.gms.common.api.Status;
+
+public class SignInActivity extends AppCompatActivity{
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
