0a3767bb221eb5d802b29334ff4bf92aa353cff7
Graham <gramclee@gmail.com>
2017-10-04 00:35:12 -0700

    Added SignInActivity to AndroidManifest.xml

--- app/src/main/java/com/ubccpsc/android/sudokuonline/SignInActivity.java
+++ app/src/main/java/com/ubccpsc/android/sudokuonline/SignInActivity.java
@@ -1,11 +1,7 @@
 package com.ubccpsc.android.sudokuonline;
 
-/**
- * Created by Graham on 2017-09-29.
- */
-
-
 import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
 
 import com.google.android.gms.auth.api.Auth;
 import com.google.android.gms.auth.api.signin.GoogleSignInAccount;
@@ -18,9 +14,17 @@ import com.google.android.gms.common.api.OptionalPendingResult;
 import com.google.android.gms.common.api.ResultCallback;
 import com.google.android.gms.common.api.Status;
 
-public class SignInActivity extends AppCompatActivity{
+public class SignInActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_in);
+
+        // Configure sign-in to request the user's ID, email address, and basic
+        // profile. ID and basic profile are included in DEFAULT_SIGN_IN.
+        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+                .requestEmail()
+                .build();
     }
 }
